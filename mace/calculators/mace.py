112c2008108928d4023189c7a663ae58b53c2519
rhyan10 <30473642+rhyan10@users.noreply.github.com>
2026-07-27 14:01:09 +0200

    Update mace.py

--- mace/calculators/mace.py
+++ mace/calculators/mace.py
@@ -117,6 +117,8 @@ class MACECalculator(Calculator):
         # SOCs & NACs: pass through as-is (units model-defined)
         socs = out["socs"].detach().to("cpu").numpy()
         nacs = out["nacs"].detach().to("cpu").numpy()
+        print(energy.shape)
+        print(nacs.shape)
 
         self.results = {
             "energy": energy,          # (n_graphs, n_states)
